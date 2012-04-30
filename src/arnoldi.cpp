1f96a6dce81fe49893b5f0077aa603854fd2c904
Robert Gantner <rngantner@gmail.com>
2012-04-30 13:38:08 +0200

    tab -> space

--- src/arnoldi.cpp
+++ src/arnoldi.cpp
@@ -48,7 +48,7 @@ void arnoldi(const MatrixBase<DerivedMatrix>& A, const MatrixBase<DerivedVector>
         H(m+1,m) = vt.norm();
         if (m != k-1)
             V.col(m+1) = vt/H(m+1,m);
-	}
+    }
 }
 
 #ifdef PYTHONMODULE
@@ -63,153 +63,153 @@ void arnoldi(const MatrixBase<DerivedMatrix>& A, const MatrixBase<DerivedVector>
  */
 template<class T>
 void call_arnoldi(PyObject* A_in, PyObject* v_in, size_t k, PyObject* V_out, PyObject* H_out) {
-	npy_intp* shape = PyArray_DIMS(A_in);
-	int n = shape[0];
-	Map< Eigen::Matrix< T,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor > > _A_in((T *) PyArray_DATA(A_in), n,n);
-	Map< Eigen::Matrix< T,Eigen::Dynamic,1 > > _v_in((T *) PyArray_DATA(v_in), n);
-	Map< Eigen::Matrix< T,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor > > _V_out((T *) PyArray_DATA(V_out), n, k);
-	Map< Eigen::Matrix< T,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor > > _H_out((T *) PyArray_DATA(H_out), k+1, k);
-	// call arnoldi algorithm. This function is also templated!
-	arnoldi(_A_in, _v_in, k, _V_out, _H_out);
+    npy_intp* shape = PyArray_DIMS(A_in);
+    int n = shape[0];
+    Map< Eigen::Matrix< T,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor > > _A_in((T *) PyArray_DATA(A_in), n,n);
+    Map< Eigen::Matrix< T,Eigen::Dynamic,1 > > _v_in((T *) PyArray_DATA(v_in), n);
+    Map< Eigen::Matrix< T,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor > > _V_out((T *) PyArray_DATA(V_out), n, k);
+    Map< Eigen::Matrix< T,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor > > _H_out((T *) PyArray_DATA(H_out), k+1, k);
+    // call arnoldi algorithm. This function is also templated!
+    arnoldi(_A_in, _v_in, k, _V_out, _H_out);
 }
 
 void arnoldi_py(PyObject* A_in, PyObject* v_in, size_t k, PyObject* V_out, PyObject* H_out) {
-	// sanity checks
-	bool error = false;
-	npy_intp* shape;
-	int d;
-	// A_in
-	d = PyArray_NDIM(A_in);
-	shape = PyArray_DIMS(A_in);
-	if (d != 2 || shape[0] != shape[1]){
-		cout << "arnoldi_py error: A is not a matrix in R^(n x n)" << endl;
-		error = true;
-	}
-	int n = shape[0];
-	// v_in
-	d = PyArray_NDIM(v_in);
-	shape = PyArray_DIMS(v_in);
-	if (d == 0 || shape[0] != n){
-		cout << "arnoldi_py error: v is not a vector in R^n" << endl;
-		error = true;
-	}
-	// V_out
-	d = PyArray_NDIM(V_out);
-	shape = PyArray_DIMS(V_out);
-	if (d != 2 || shape[0] != n || (unsigned int)shape[1] != k){
-		cout << "arnoldi_py error: V is not a matrix in R^(n x k)" << endl;
-		error = true;
-	}
-	// H_out
-	d = PyArray_NDIM(H_out);
-	shape = PyArray_DIMS(H_out);
-	if (d != 2 || (unsigned int)shape[0] != k+1 || (unsigned int)shape[1] != k){
-		cout << "arnoldi_py error: H is not a matrix in R^(k+1 x k)" << endl;
-		error = true;
-	}
-	
-	if (error) return;
-	
-	// determine numpy data type
-	PyArray_Descr* descr = PyArray_DESCR(A_in);
-	switch (descr->type) {
-	// float32 (single precision)
-	case 'f':
-		call_arnoldi<float>(A_in, v_in, k, V_out, H_out);
-	break;
-	
-	// float64 (double precision)
-	case 'd':
-		call_arnoldi<double>(A_in, v_in, k, V_out, H_out);
-	break;
-	
-	// complex64 (2x single precision)
-	case 'F':
-		call_arnoldi<std::complex<float> >(A_in, v_in, k, V_out, H_out);
-	break;
-	
-	// complex128 (2x double precision)
-	case 'D':
-		call_arnoldi<std::complex<double> >(A_in, v_in, k, V_out, H_out);
-	break;
-		
+    // sanity checks
+    bool error = false;
+    npy_intp* shape;
+    int d;
+    // A_in
+    d = PyArray_NDIM(A_in);
+    shape = PyArray_DIMS(A_in);
+    if (d != 2 || shape[0] != shape[1]){
+        cout << "arnoldi_py error: A is not a matrix in R^(n x n)" << endl;
+        error = true;
+    }
+    int n = shape[0];
+    // v_in
+    d = PyArray_NDIM(v_in);
+    shape = PyArray_DIMS(v_in);
+    if (d == 0 || shape[0] != n){
+        cout << "arnoldi_py error: v is not a vector in R^n" << endl;
+        error = true;
+    }
+    // V_out
+    d = PyArray_NDIM(V_out);
+    shape = PyArray_DIMS(V_out);
+    if (d != 2 || shape[0] != n || (unsigned int)shape[1] != k){
+        cout << "arnoldi_py error: V is not a matrix in R^(n x k)" << endl;
+        error = true;
+    }
+    // H_out
+    d = PyArray_NDIM(H_out);
+    shape = PyArray_DIMS(H_out);
+    if (d != 2 || (unsigned int)shape[0] != k+1 || (unsigned int)shape[1] != k){
+        cout << "arnoldi_py error: H is not a matrix in R^(k+1 x k)" << endl;
+        error = true;
+    }
+    
+    if (error) return;
+    
+    // determine numpy data type
+    PyArray_Descr* descr = PyArray_DESCR(A_in);
+    switch (descr->type) {
+    // float32 (single precision)
+    case 'f':
+        call_arnoldi<float>(A_in, v_in, k, V_out, H_out);
+    break;
+    
+    // float64 (double precision)
+    case 'd':
+        call_arnoldi<double>(A_in, v_in, k, V_out, H_out);
+    break;
+    
+    // complex64 (2x single precision)
+    case 'F':
+        call_arnoldi<std::complex<float> >(A_in, v_in, k, V_out, H_out);
+    break;
+    
+    // complex128 (2x double precision)
+    case 'D':
+        call_arnoldi<std::complex<double> >(A_in, v_in, k, V_out, H_out);
+    break;
+        
 //// unsupported types
-//	case 'e': // float 16
-//	call_arnoldi<float16>(A_in, v_in, k, V_out, H_out);
-//	break;
-//	case 'g': // floa128 (quadruple precision)
-//		call_arnoldi<float>(A_in, v_in, k, V_out, H_out);
-//	break;
-//	case 'G': // complex256 (2x quadruple precision)
-//		call_arnoldi<complex256>(A_in, v_in, k, V_out, H_out);
-//	break;
-		
-		// default
-	default:
-		cout << "arnoldi_py error: unknown type: " << descr->type << endl;
-		return;
-	}
+//    case 'e': // float 16
+//    call_arnoldi<float16>(A_in, v_in, k, V_out, H_out);
+//    break;
+//    case 'g': // floa128 (quadruple precision)
+//        call_arnoldi<float>(A_in, v_in, k, V_out, H_out);
+//    break;
+//    case 'G': // complex256 (2x quadruple precision)
+//        call_arnoldi<complex256>(A_in, v_in, k, V_out, H_out);
+//    break;
+        
+        // default
+    default:
+        cout << "arnoldi_py error: unknown type: " << descr->type << endl;
+        return;
+    }
 }
 
 namespace bp = boost::python;
 #include "boost/python.hpp"
 BOOST_PYTHON_MODULE(arnoldi) {
-	//bp::scope().attr("__doc__") = "Arnoldi algorithm (Krylov approximation of a matrix)\n    input:\n        A: matrix to approximate\n        v0: initial vector (should be in matrix form)\n        k: number of Krylov steps\n    output:\n        V: matrix (large, N*k) containing the orthogonal vectors\n        H: matrix (small, k*k) containing the Krylov approximation of A";
-	bp::numeric::array::set_module_and_type("numpy", "ndarray");
-	
-	bp::def("arnoldi", arnoldi_py, "Arnoldi algorithm (Krylov approximation of a matrix)\n    input:\n        A: matrix to approximate\n        v0: initial vector (should be in matrix form)\n        k: number of Krylov steps\n    output:\n        V: matrix (large, N*k) containing the orthogonal vectors\n        H: matrix (small, k*k) containing the Krylov approximation of A\n\nExample:\n    >>> A=rand(10,10); v=rand(10,1)\n    >>> V=zeros((10,5)); H=zeros((6,5))\n    >>> arnoldi(A,v,5,V,H)");
+    //bp::scope().attr("__doc__") = "Arnoldi algorithm (Krylov approximation of a matrix)\n    input:\n        A: matrix to approximate\n        v0: initial vector (should be in matrix form)\n        k: number of Krylov steps\n    output:\n        V: matrix (large, N*k) containing the orthogonal vectors\n        H: matrix (small, k*k) containing the Krylov approximation of A";
+    bp::numeric::array::set_module_and_type("numpy", "ndarray");
+    
+    bp::def("arnoldi", arnoldi_py, "Arnoldi algorithm (Krylov approximation of a matrix)\n    input:\n        A: matrix to approximate\n        v0: initial vector (should be in matrix form)\n        k: number of Krylov steps\n    output:\n        V: matrix (large, N*k) containing the orthogonal vectors\n        H: matrix (small, k*k) containing the Krylov approximation of A\n\nExample:\n    >>> A=rand(10,10); v=rand(10,1)\n    >>> V=zeros((10,5)); H=zeros((6,5))\n    >>> arnoldi(A,v,5,V,H)");
 }
 #endif
 
 int main (int argc, char const *argv[])
 {
-	size_t n = 8;
-	
-	// initialize A
-	Types<double>::Matrix A(n,n);
-	for (unsigned int i=0; i<n; ++i) {
-		A(i,i) = 2;
-	}
-	for (unsigned int i=1; i<n; ++i) {
-		A(i,i-1) = -1;
-		A(i-1,i) = -1;
-	}
-	cout << "A:\n" << A << endl;
-	
-	// initialize v
-	Types<double>::Vector v(n);
-	for (unsigned int i=0; i<n; ++i)
-		v(i) = 1;
-	
-	// initialize H and V
-	int m = 4;
-	Types<double>::Matrix V(n,m),H(m+1,m);
-	
-	// call arnoldi
-	arnoldi(A, v, m, V, H);
-	
-//	cout << "A:\n" << A << endl;
-//	cout << "V:\n" << V << endl;
-	cout << "H:\n" << H << endl;
-	//cout << "V^T * A * V:\n" << V.transpose()*A*V << endl;
-	
-	// compute exponential
-	Types<double>::Matrix expA(n,n);
-	Types<double>::Matrix expH(m,m);
-	
-	Eigen::MatrixExponential<Types<double>::Matrix> expa(A);
-	Eigen::MatrixExponential<Types<double>::Matrix> exph(H.block(0,0,m,m));
-	expa.compute(expA); // stores result in expM
-	exph.compute(expH); // stores result in expM
-	//cout << "expA:\n" << expA << endl;
-	//cout << "expH:\n" << expH << endl;
-	
-	//cout << "V^T * H * V:\n" << expA << endl;
-	cout << "V^T * A * V:\n" << V.transpose()*A*V << endl;
-	//cout << "V^T * expH * V:\n" << V*expH*V.transpose() << endl;
-	
-	return 0;
+    size_t n = 8;
+    
+    // initialize A
+    Types<double>::Matrix A(n,n);
+    for (unsigned int i=0; i<n; ++i) {
+        A(i,i) = 2;
+    }
+    for (unsigned int i=1; i<n; ++i) {
+        A(i,i-1) = -1;
+        A(i-1,i) = -1;
+    }
+    cout << "A:\n" << A << endl;
+    
+    // initialize v
+    Types<double>::Vector v(n);
+    for (unsigned int i=0; i<n; ++i)
+        v(i) = 1;
+    
+    // initialize H and V
+    int m = 4;
+    Types<double>::Matrix V(n,m),H(m+1,m);
+    
+    // call arnoldi
+    arnoldi(A, v, m, V, H);
+    
+//    cout << "A:\n" << A << endl;
+//    cout << "V:\n" << V << endl;
+    cout << "H:\n" << H << endl;
+    //cout << "V^T * A * V:\n" << V.transpose()*A*V << endl;
+    
+    // compute exponential
+    Types<double>::Matrix expA(n,n);
+    Types<double>::Matrix expH(m,m);
+    
+    Eigen::MatrixExponential<Types<double>::Matrix> expa(A);
+    Eigen::MatrixExponential<Types<double>::Matrix> exph(H.block(0,0,m,m));
+    expa.compute(expA); // stores result in expM
+    exph.compute(expH); // stores result in expM
+    //cout << "expA:\n" << expA << endl;
+    //cout << "expH:\n" << expH << endl;
+    
+    //cout << "V^T * H * V:\n" << expA << endl;
+    cout << "V^T * A * V:\n" << V.transpose()*A*V << endl;
+    //cout << "V^T * expH * V:\n" << V*expH*V.transpose() << endl;
+    
+    return 0;
 }
 
 
-#endif	/* ARNOLDI_HPP */
+#endif    /* ARNOLDI_HPP */
